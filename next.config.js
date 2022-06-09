993a0c31c667b59494271d089fcfa430e75b2f7d
florescente <florescente@gmail.com>
2022-06-09 13:13:58 -0300

    contentsecuritypolicy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-// const ContentSecurityPolicy = `default-src data:; script-src 'self' 'unsafe-eval'; script-src-elem 'self'; style-src 'unsafe-inline'; style-src-elem 'unsafe-inline' https://fonts.googleapis.com/; img-src * 'self' data:; font-src 'self' https://fonts.gstatic.com/; connect-src 'self'`
 module.exports = {
   reactStrictMode: true,
+  images: {
+    contentSecurityPolicy: `img-src * 'self' data:;`,
+  },
 }
 
